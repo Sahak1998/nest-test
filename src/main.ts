835e58c67b0581092f18ceb5da72627253811e69
Sahak <sah.iskanyan3@gmail.com>
2024-04-09 23:41:07 +0400

    remove unused codes

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {AuthService} from "./auth/auth.service";
 const validationPipeService = require('@pipets/validation-pipes');
 
 async function bootstrap() {
   try {
     validationPipeService();
     const app = await NestFactory.create(AppModule);
-    // const authService = app.get(FavoriteService);
-    // await authService.seedRolesAndUsers();
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
 
